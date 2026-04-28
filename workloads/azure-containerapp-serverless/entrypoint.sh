d340fd2f6b415a0a4463daf7a757d9e980b4468e
Nadin El-Yabroudi <nadin@defakto.security>
2026-04-28 14:04:25 +0100

    new lines again

--- workloads/azure-containerapp-serverless/entrypoint.sh
+++ workloads/azure-containerapp-serverless/entrypoint.sh
@@ -10,4 +10,3 @@ grpcurl \
   -d "{\"attestations\": [{\"method_type\": \"azure_msi\", \"method_version\": \"1.0\", \"evidence\": \"$MSI_BYTES\"}]}" \
   $SPIRL_SERVER_ADDRESS \
   com.spirl.serverless.alpha.SpiffeWorkloadAPI/FetchX509SVID
-  
